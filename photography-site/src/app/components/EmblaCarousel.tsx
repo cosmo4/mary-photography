21311ac923eb84baba5385c3404c4f4a5b322d10
Luke Warner <cosmo4@byui.edu>
2024-11-16 22:42:21 -0700

    carousel responsive to user mouse actions, styles added

--- photography-site/src/app/components/EmblaCarousel.tsx
+++ photography-site/src/app/components/EmblaCarousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { EmblaOptionsType } from 'embla-carousel';
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
@@ -19,6 +19,24 @@ type PropType = {
 const EmblaCarousel: React.FC<PropType> = ({ options }) => {
   const [images, setImages] = useState<ImageData[]>([]);
   const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay({ delay: 5000, stopOnInteraction: false })]);
+  const containerRef = useRef<HTMLElement | null>(null);
+
+  // Callback to stop autoplay
+  const handleMouseEnter = useCallback(() => {
+    if (!emblaApi) return;
+
+    emblaApi.plugins().autoplay.stop();
+  }, [emblaApi]);
+
+  // Callback to stop autoplay
+  const handleMouseLeave = useCallback(() => {
+    if (!emblaApi) return;
+
+    emblaApi.plugins().autoplay.play();
+
+    // Jump to the next slide
+    emblaApi.scrollNext();
+  }, [emblaApi]);
 
   // Fetch images from JSON file on mount
   useEffect(() => {
@@ -31,22 +49,38 @@ const EmblaCarousel: React.FC<PropType> = ({ options }) => {
     fetchImages();
   }, []);
 
+  // Effect to stop and start auto play when the mouse is hovering or not over the container
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container) {
+        container.addEventListener('mouseenter', handleMouseEnter);
+        container.addEventListener('mouseleave', handleMouseLeave);
+
+        // cleanup after component unmounts
+        return () => {
+            container.removeEventListener('mouseenter', handleMouseEnter);
+            container.removeEventListener('mouseleave', handleMouseLeave);
+        };
+    }
+}, [handleMouseLeave, handleMouseEnter]);
+
   const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
 
   return (
-    <section className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container h-75vh w-full">
-          {images.map((image, index) => (
-            <div className="embla__slide relative" key={index}>
-              <Image src={image.src} alt={image.alt} layout='fill' objectFit='cover' className="embla__slide__img" />
-            </div>
-          ))}
-        </div>
-      </div>
+    <section className="embla" ref={containerRef}>
       <div className="embla__controls">
-        <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-        <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+      <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container h-75vh w-full">
+            {images.map((image, index) => (
+              <div className="embla__slide relative" key={index}>
+                <Image src={image.src} alt={image.alt} layout='fill' objectFit='cover' className="embla__slide__img" />
+              </div>
+            ))}
+          </div>
+        </div>
+          
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
       </div>
     </section>
   );
