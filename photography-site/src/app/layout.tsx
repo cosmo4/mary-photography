3283aaa6d431f4350a11568734a186ad1e114346
Luke Warner <cosmo4@byui.edu>
2024-12-13 05:06:23 -0700

    added vercel speed insights

--- photography-site/src/app/layout.tsx
+++ photography-site/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from './components/shared/Header';
 import Footer from './components/shared/Footer';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Mary Mills Photography",
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body
         className={`antialiased`}
       >
+        <SpeedInsights/>
         <Header />
           <main className="pt-[130px] w-full">{children}</main>
         <Footer />
